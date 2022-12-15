18d60caf96796d68a06b70ad0d7081c1f6ec79b3
Luca Park <qlife1146@gmail.com>
2022-12-16 02:33:59 +0900

    test

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,7 @@ class App extends StatelessWidget {
                   ),
                 ],
                 mainAxisAlignment: MainAxisAlignment.center,
-              )
+              ),
             ],
           ),
         ),
